cc4d0d07f44214aa43443644857d17797edad2ff
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-06-27 22:23:54 -0300

    trabalhando com classes

---
+++ Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Classes/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos.Fundamentos.Console.Classes
+{
+    public class Car
+    {
+        private string _carName;
+        private string _color;
+        public Car(string carName, string color)
+        {
+          _color = color;
+          _carName = carName;
+        }
+
+        public string Describe()
+        {
+            return $"This car is {_color}";
+        }
+
+        public string CarName { get { return _carName; } set { _carName = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+    }
+}
