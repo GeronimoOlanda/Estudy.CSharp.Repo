33ad9a66982ccc8d57a0c1867d94112af17cd484
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-06-28 22:52:12 -0300

    up

--- Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Classes/Car.cs
+++ Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Classes/Car.cs
@@ -10,10 +10,12 @@ namespace Estudos.Fundamentos.Console.Classes
     {
         private string _carName;
         private string _color;
-        public Car(string carName, string color)
+        private string _model;
+        public Car(string carName, string color, string model)
         {
           _color = color;
           _carName = carName;
+          _model = model;
         }
 
         public string Describe()
@@ -21,6 +23,10 @@ namespace Estudos.Fundamentos.Console.Classes
             return $"This car is {_color}";
         }
 
+        public string DescribeModel()
+        {
+            return $"This i the model = {_model}";
+        }
         public string CarName { get { return _carName; } set { _carName = value; } }
         public string Color { get { return _color; } set { _color = value; } }
     }
--- Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Program.cs
+++ Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Estudos.Fundamentos.Console.Classes;
+using System.Collections;
+
+Car car = new Car("Ferrari", "Black", "Adunno");
+
+Console.WriteLine(car.CarName);
+Console.WriteLine(car.Color);
+Console.WriteLine(car.Describe());
+Console.WriteLine(car.DescribeModel());
+
+
