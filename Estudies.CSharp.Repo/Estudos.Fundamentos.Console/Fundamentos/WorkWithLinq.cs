fd1c1fa6a251f054a2df89c8dc3815aa35d0aa22
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-07-05 00:34:37 -0300

    trabalhando co LINQ

---
+++ Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Fundamentos/WorkWithLinq.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estudos.Fundamentos.Console.Classes;
+using System.Collections;
+
+namespace Estudos.Fundamentos.Console.Fundamentos
+{
+    internal class WorkWithLinq
+    {
+
+        public WorkWithLinq()
+        {
+
+        }
+
+
+        public void WorkWhere()
+        {
+    
+
+            var pessoa = new List<Pessoa>();
+
+            pessoa.Add(new Pessoa() { Id = 1, Name = "Geronimo", Age = 24 });
+            pessoa.Add(new Pessoa() { Id = 2, Name = "Thalia", Age = 23 });
+            pessoa.Add(new Pessoa() { Id = 3, Name = "Alice", Age = 22 });
+            pessoa.Add(new Pessoa() { Id = 4, Name = "Milena", Age = 41 });
+            pessoa.Add(new Pessoa() { Id = 5, Name = "Emannuel", Age = 66 });
+            pessoa.Add(new Pessoa() { Id = 6, Name = "Gaspar", Age = 12 });
+
+
+            var exibir = pessoa.Where(p => p.Id > 0).Where(p => p.Id < 6);
+            foreach (var ex in exibir)
+            {
+                System.Console.WriteLine($"Id = {ex.Id}. Nome = {ex.Name}. Idade = {ex.Age}.");
+
+            }
+
+        }
+
+    }
+
+
+}
