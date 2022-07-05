9b71359a259d3cb36e91b785ba808beb63c32aa2
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-07-05 00:34:55 -0300

    chamando noso metodo LINQ na classe program

--- Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Program.cs
+++ Estudies.CSharp.Repo/Estudos.Fundamentos.Console/Program.cs
@@ -1,12 +1,6 @@
-﻿using Estudos.Fundamentos.Console.Classes;
-using System.Collections;
+﻿using Estudos.Fundamentos.Console.Fundamentos;
 
+WorkWithLinq work = new WorkWithLinq();
 
-Car car = new Car("Ferrari", "Black", "Adunno");
-
-Console.WriteLine(car.CarName);
-Console.WriteLine(car.Color);
-Console.WriteLine(car.Describe());
-Console.WriteLine(car.DescribeModel());
-
-
+work.WorkWhere();
+Console.WriteLine("--------------------------------------");
